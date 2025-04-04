8a78385f014db484ed0552996a1b1eb95c3317aa
Michee Luzayisu <michemakaya11@gmail.com>
2025-04-04 18:51:40 +0200

    Add more TabButton

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,10 @@ function App() {
         <section id="examples">
           <h2>Example</h2>
           <menu>
-            <TabButton>Component</TabButton>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
           </menu>
         </section>
       </main>
